191011d678b3e883d9636f6d8fd9148a3c395b6a
Thomas Roughton <t.roughton@me.com>
2016-05-12 15:57:46 +1200

    Updated to yet another version of the Optional syntax.

--- Tools/main.swift
+++ Tools/main.swift
@@ -370,23 +370,23 @@ func paramType(x:KhronosXmlDelegate.paramTuple) -> String
     } else if type == "struct _cl_event" {
         type = "OpaquePointer"
     } else if x.ptr == "const!*?" {
-        type = "UnsafePointer<\(type)>!"
+        type = "UnsafePointer<\(type)>?"
     } else if x.ptr == "!*?" {
-        type = "UnsafeMutablePointer<\(type)>!"
+        type = "UnsafeMutablePointer<\(type)>?"
     } else if x.ptr == "void*?" {
-        type = "UnsafeMutablePointer<Void>!"
+        type = "UnsafeMutablePointer<Void>?"
     } else if x.ptr == "constvoid*?" {
-        type = "UnsafePointer<Void>!"
+        type = "UnsafePointer<Void>?"
     } else if x.ptr == "constvoid**?" {
-        type = "UnsafeMutablePointer<UnsafePointer<Void>?>!"
+        type = "UnsafeMutablePointer<UnsafePointer<Void>?>?"
     } else if x.ptr == "const!*const*?" {
-        type = "UnsafePointer<UnsafePointer<\(type)>!>!"
+        type = "UnsafePointer<UnsafePointer<\(type)>?>?"
     } else if x.ptr == "const!**?" {
-        type = "UnsafeMutablePointer<UnsafePointer<\(type)>?>!"
+        type = "UnsafeMutablePointer<UnsafePointer<\(type)>?>?"
     } else if x.ptr == "void**?" {
-        type = "UnsafeMutablePointer<UnsafeMutablePointer<Void>?>!"
+        type = "UnsafeMutablePointer<UnsafeMutablePointer<Void>?>?"
     } else if x.ptr == "constvoid*const*?" {
-        type = "UnsafePointer<UnsafePointer<Void>!>!"
+        type = "UnsafePointer<UnsafePointer<Void>?>?"
     }
     // Helper to find new pointer types
     // else if x.ptr != "!?" {
